1bc8c506326c890ce3fce9c9cb5dd34337f65d99
Titouan Renard <titouan.renard@gmail.com>
2020-11-12 11:48:50 +0100

    nicer class structure

--- Functions/Global.py
+++ Functions/Global.py
@@ -1,43 +1,40 @@
 """ Developped by: Titouan """
 
 import matplotlib.pyplot as plt
-import functools
+from shapely import geometry as geo
 import itertools
 import operator
+import copy
 
 """ just returns a test map so that test runs of the planning algorithm may be computed """
 def TestMap():
-    polyA = [[40,30],
-             [40,50],
-             [45,40],
-             [45,35]]
+    polyA = geo.Polygon([(40.,30.),
+                         (40.,50.),
+                         (45.,40.),
+                         (45.,35.)])
 
-    polyB = [[70,35],
-             [60,46],
-             [50,63],
-             [70,55]]
+    polyB = geo.Polygon([(70.,35.),
+                         (60.,46.),
+                         (50.,63.),
+                         (70.,55.)])
 
-    polyC = [[35,60],
-             [35,70],
-             [42,76],
-             [45,60]]
+    polyC = geo.Polygon([(35.,60.),
+                         (35.,70.),
+                         (42.,76.),
+                         (45.,60.)])
 
     return [polyA,polyB,polyC]
 
 """ placeholder map plotting function """
-def mapPlot(map,start,finish):
+def mapPlot(map,start,finish,debugLines=False):
     #plotting parameters
     fsz = 8 # size of the plotted figure
     polyColor = "blue" # color of the map polygons
     startColor = "green" # color of the starting point
     finishColor = "red" # color the the finish point
+    debugLineColor = "black"
 
     #defining a function to plot a polygon :
-    def polyPlot(poly,color):
-        for i in range(1,len(poly)):
-            plt.plot([ poly[i-1][0],poly[i][0] ],[ poly[i-1][1],poly[i][1] ],color=color)
-        plt.plot([ poly[0][0],poly[len(poly)-1][0] ],[ poly[0][1],poly[len(poly)-1][1] ], color=color)
-
     #setting map limits
     plt.xlim(0,100)
     plt.ylim(0,100)
@@ -51,14 +48,18 @@ def mapPlot(map,start,finish):
 
     ## plotting polygons
     for poly in map:
-        polyPlot(poly,polyColor)
+        plt.plot(*poly.exterior.xy,color=polyColor)
     
     #Displaying start and finish
     startCircle = plt.Circle(start,radius=1,color=startColor)
     ax.add_artist(startCircle)
-
     finishCircle = plt.Circle(finish,radius=1,color=finishColor)
     ax.add_artist(finishCircle)
+
+    #Plotting the debug lines
+    if debugLines != False:
+        for line in debugLines:
+            plt.plot([ line[0][0],line[1][0] ],[ line[0][1],line[1][1] ],color=debugLineColor,linestyle="dashed")
 
     #displaying the map
     plt.show()
@@ -66,20 +67,52 @@ def mapPlot(map,start,finish):
 """ Global Navigation Class """
 class Global:
     """ Handles global path planning """
-    i = 12345
 
+    #constructor
     def __init__(self, polyMap, start, finish):
-        self.map = polyMap
+        self.polyMap = polyMap
         self.start = start
         self.finish = finish
 
-    def plot(self):
-        mapPlot(self.map,self.start,self.finish)
+    #plots the map and paths
+    def plot(self,debugLines=False):
+        mapPlot(self.polyMap,self.start,self.finish,debugLines)
 
-    def vPaths(self):
-        vertices = functools.reduce(operator.iconcat,self.map)
-        paths = list(itertools.combinations(vertices,2))
-        print(paths)
+    # Computes possible vertex the robot can drive to (first vertex in array is start, last is finish)
+    def navPoints(self):
+        #print(list(self.polyMap[i].exterior.coords))
+        polyVertices = [list(poly.exterior.coords) for poly in self.polyMap]
+        mapVertices = [y for x in polyVertices for y in x]
+        vertices = copy.deepcopy(mapVertices)
+        vertices.insert(0,self.start)
+        vertices.append(self.finish)
+        
+        return vertices
+
+    #returns all segements of the map polygons (usefull for intersection)
+    def mapSegments(self):
+        polyVertices = [list(poly.exterior.coords) for poly in self.polyMap]
+        mapVertices = [y for x in polyVertices for y in x]
+        segments = list(itertools.combinations(mapVertices,2))
+        return segments
+
+
+    #computes all possible paths
+    def paths(self):
+        def visiblePaths(unvisited):
+            mapSeg = self.mapSegments()
+            current = unvisited.pop(0)
+            paths = []
+            for succ in unvisited:
+                newSeg = (current,succ)
+                paths.append(newSeg)
+            return paths
+
+        unvisited = self.navPoints();
+        paths = visiblePaths(unvisited)
+        
+
+        return paths
 
     def returnPath(self,map,startPoint,endPoint):
         """ takes map: an array of scipy polygons, startPoint 
@@ -92,9 +125,9 @@ class Global:
 
 #print(test.f())
 
-start = (80,30)
-finish = (20,70)
+start = (80.,30.)
+finish = (20.,70.)
 
 test = Global(TestMap(),start,finish)
 
-test.vPaths()
+test.plot(test.paths())
