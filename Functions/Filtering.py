a4e276716acb3120e67ae765ffefd2743c0732da
Chassagne <pierre.chassagne@epfl.ch>
2020-11-14 12:31:40 +0100

    flitering as class finally + added txt file with thymio variables

--- Functions/Filtering.py
+++ Functions/Filtering.py
@@ -10,16 +10,25 @@ from Thymio import Thymio
 from Timer import RepeatedTimer
 from tqdm import tqdm
 
+class Filtering:
 
-def getData():
-    thymio_data.append({"ground":th["prox.ground.reflected"], 
-                        "sensor":th["prox.ground.reflected"],
-                        "left_speed":th["motor.left.speed"],
-                        "right_speed":th["motor.right.speed"]})
+    def __init__(RspeedVar, LspeedVar, thetaVar):
+        # Stock the variances obtain with precedent 
+        self.RspeedVar = RspeedVar
+        self.LspeedVar = LspeedVar
+        self.thetaVar = thetaVar
+
+
+    @staticmethod
+    def getData():
+        thymio_data.append({"ground":th["prox.ground.reflected"], 
+                            "sensor":th["prox.ground.reflected"],
+                            "left_speed":th["motor.left.speed"],
+                            "right_speed":th["motor.right.speed"]})
     
 
-def filter(self,camera_position,state_estimator,target = False):
-    """ input:      the position as computed by the vision algorithm (a 3d numpy vector)
-                    the position estimated by the state estimator (a 3d numpy vector), 
+    def filter(self,enable,camera_position,state_estimator,target = False):
+        """ input:      the position as computed by the vision algorithm (a 3d numpy vector)
+                        the position estimated by the state estimator (a 3d numpy vector), 
 
-        returns:    an estimation of position a 3d numpy vector """
+            returns:    an estimation of position a 3d numpy vector """
