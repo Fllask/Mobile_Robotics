b62dd82876a08834ecc372d9ae6294d28e0b3b22
Chassagne <pierre.chassagne@epfl.ch>
2020-11-15 19:30:19 +0100

    Begin filtering part (it seems to work) + modified a bit the robot class to add the state space model

--- Functions/Robot.py
+++ Functions/Robot.py
@@ -1,17 +1,13 @@
 """ Developped by: Thomas """
 class Robot:
     """ Handles the control of the robot """
-    i = 12345
+    
     #test
-    def __init__(self):
-        self.data = []
+    def __init__(self, A, B):
+        self.A = A
+        self.B = B
 
-    def f(self):
-        return 'hello world'
 
     def stateEstimator(self):
         """ returns: coordonées en x,y,theta globales (un vecteur 3d numpy) """
         return ""
-
-test = Robot()
-print(test.f())
