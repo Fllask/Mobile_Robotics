a6059646496aabb2c4f9ec6e2e3ab7eb823aeeab
Titouan Renard <titouan.renard@gmail.com>
2020-11-29 16:10:00 +0100

    actually mutlithreads it is very nice

--- Functions/main.py
+++ Functions/main.py
@@ -1,13 +1,20 @@
 #!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8
 #the line above makes it more nicely executable on my mac, PATH may differ on different machines
 
+"""
+
+    The way the main module works is we define a bunch of functions in classes that in turn 
+    get instanciated as different processes by the multiprocess module, this allows
+    us to implement true parralelism (something that threads are not able to do in cPython)
+
+"""
+
 # general includes (os stuff so that we can do multiprocesses)
 import os #to access os parameters (Process Ids mostly)
 import sys
 from multiprocessing import * # I use the multiprocessing library for parallelism ==> https://docs.python.org/3.8/library/multiprocessing.html
 import time #to time routines and have some idea of what is slow
 import cv2
-print('OpenCL available:', cv2.ocl.haveOpenCL())
 if cv2.ocl.haveOpenCL():
     cv2.ocl.setUseOpenCL(True)
 import numpy as np
@@ -21,24 +28,6 @@ from Utilities import Utilities
 from Global import Global
 import Vision as v
 
-"""
-
-    The way this file works is we define a bunch of functions that in turn 
-    get instanciated as different processes by the multiprocess module, this allows
-    us to implement true parralelism (something that threads are not able to do in cPython)
-
-"""
-
-# Image processing process
-def imageProc():
-    #process some images idk not my part
-    time.sleep(0.1)
-
-# Handles the windows and the overlays:
-def display():
-    #spawn windows 
-    time.sleep(0.1)
-
 """ 
     ComputeVision:
     Handles the vision and global planning as a process
@@ -51,9 +40,12 @@ class ComputeVision():
 
 
     """ Starts the vision process """
-    def run(self):
-        mainLoopProcess = Process(target=self.mainLoop, args=())
-        mainLoopProcess.start()
+    def run(self,d):
+        self.mainLoopProcess = Process(target=self.mainLoop, args=(d,))
+        self.mainLoopProcess.start()
+    
+    def join(self):
+        self.mainLoopProcess.join()
 
     """ Image Loading Function """
     def loadImg(self):
@@ -68,16 +60,19 @@ class ComputeVision():
     def display(self):
         tr_img = cv2.warpPerspective(self.img, self.vis.trans, (1000,1000))
 
+        cv2.drawContours(tr_img, self.vis.getMap(downscale=False), -1, (0,255,0), 3)
+
         path = self.g.path
         for i in range(1,len(path)) :
             cv2.line(tr_img,(int(path[i][0]*10),int(path[i][1]*10)),(int(path[i-1][0]*10),int(path[i-1][1]*10)),(255,0,0),thickness=3)
-        cv2.drawContours(tr_img, self.vis.getMap(downscale=False), -1, (0,255,0), 3)
 
         return tr_img
 
         
     """ Main loop for vision """
-    def mainLoop(self):
+    def mainLoop(self,d):
+        if self.verbose:
+            print("Starting vision + global navigation main loop")
         # initialization of the vision object
         self.img = self.loadImg()
         t0 = time.process_time()
@@ -89,14 +84,17 @@ class ComputeVision():
         t0 = time.process_time()
         rbt = self.vis.returnDynamicCoordinates() ## getting robot coordinate
         self.rob = (rbt[0][0]/10,rbt[0][1]/10)
+        d['pos'] = self.rob
         if self.verbose:
             print("Initial Robot_Pos Estimation Time : "+str(time.process_time()-t0))
         
         #initializing the path planning module
         t0 = time.process_time()
         self.obstacles = self.vis.getMap()
+        d['map'] = self.obstacles
         self.g = Global(self.obstacles,(float(self.rob[0]),float(self.rob[1])),(5.,5.))
         self.path = self.g.plotPath(plotGraph=False,plotMap=False)
+        d['path'] = self.path
         if self.verbose:
             print("Initial Path Planning Time : "+str(time.process_time()-t0))
 
@@ -110,6 +108,7 @@ class ComputeVision():
             ## getting robot coordinates
             rbt = self.vis.returnDynamicCoordinates() 
             self.rob = (rbt[0][0]/10,rbt[0][1]/10)
+            d['pos'] = self.rob
             
             ## computing path
             #self.path = self.g.plotPath(plotGraph=False,plotMap=False)
@@ -123,6 +122,7 @@ class ComputeVision():
             
             if self.verbose:
                 print("Full Vision Loop : "+str(time.process_time()-t0))
+            d['vtime']=str(time.process_time()-t0)
 
 
 """ 
@@ -133,28 +133,51 @@ class ComputeVision():
 class RobotControl():
     """ Constructor of the RobotControl class """
     def __init__(self,verbose,address):
-        self.verbose = verbose
+        self.verbose = True
+        self.address = address
+
+        
+
+    """ Starts the control process """
+    def run(self,d):
+        self.mainLoopProcess = Process(target=self.mainLoop, args=(d,))
+        self.mainLoopProcess.start()
+
+    def join(self):
+        self.mainLoopProcess.join()
+
+    """ Main control loop """
+    def mainLoop(self,d):
+
+        t0 = time.process_time()
+        if self.verbose:
+            print("Starting control main loop")
+        t = 0        
 
         if self.verbose:
-            print("Connecting to Thymio at address "+address+" ... ")
+            print("Connecting to Thymio at address "+self.address+" ... ")
         try:
-            self.th = Thymio.serial(port=address, refreshing_rate=0.1)
+            self.th = Thymio.serial(port=self.address, refreshing_rate=0.1)
             time.sleep(3)
             self.th.set_var_array("leds.top", [0, 0, 0])
             if self.verbose:
                 print("connection successful !")
         except:
             print("Unexpected error:", sys.exc_info()[0])
-            raise
+            return
 
-    """ Starts the control process """
-    def run(self):
-        mainLoopProcess = Process(target=self.mainLoop, args=())
-        mainLoopProcess.start()
-
-    """ Main control loop """
-    def mainLoop(self):
-        print("LOOP")
+        if self.verbose:
+            print("Starting main loop")
+        while True:
+            rt = time.process_time() - t0
+            t0 = time.process_time()
+            if int(round(time.process_time(),0)) > t:
+                t = int(round(time.process_time(),0))
+                nd = dict(d)
+                if self.verbose:
+                    print("control period : " + str(rt))
+                    print("vision period : "+d["vtime"])
+                    print("position of robot : "+str(nd['pos']))
 
             
 
@@ -163,6 +186,8 @@ class RobotControl():
     @autor: Titou
 """
 if __name__ == '__main__':
+
+    print('OpenCL available:', cv2.ocl.haveOpenCL())
 
 
     """ Parsing stdin """
@@ -177,7 +202,17 @@ if __name__ == '__main__':
     except:
         sys.exit(1)
 
-    cmptVis = ComputeVision(verbose)
+    manager = Manager()
 
-    # cmptVis.run()
-    ctrl.run()
+    d = manager.dict()
+    d['pos'] = False
+    d['path'] = False
+    d['map'] = False
+    d['vtime'] = "0"
+
+    cmptVis = ComputeVision(False)
+
+    cmptVis.run(d)
+    ctrl.run(d)
+    cmptVis.join()
+    ctrl.join()
