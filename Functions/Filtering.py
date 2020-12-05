4f6990307a153cbcb9b1b0de399006a29b87554c
Titouan Renard <titouan.renard@gmail.com>
2020-12-05 18:57:22 +0100

    cleanup

--- Functions/Filtering.py
+++ Functions/Filtering.py
@@ -33,12 +33,11 @@ class Filtering:
         innovation = zk - np.dot(H,X_est)
        
         if update_cam:
-            
             innovation[2]= (m.pi+innovation[2])%(2*m.pi)-m.pi
-            print('inn\n', innovation)
+
         S = np.dot(H, np.dot(P_est_priori, H.T)) + R
         K = np.dot(P_est_priori, np.dot(H.T, np.linalg.inv(S)))
-        #print('K\n',K)
+
         X_est = X_est + np.dot(K,innovation)
         P_est = P_est_priori - np.dot(K,np.dot(H, P_est_priori))
         return X_est, P_est
@@ -155,7 +154,5 @@ class Filtering:
             self.Q[4][2] = self.Q[2][4]
             self.Q[4][3] = self.Q[3][4]
             self.Q[4][4] = sig*Ts
-
-            print(self.Q)
        return self.Q
 
--- Functions/Robot.py
+++ Functions/Robot.py
@@ -168,7 +168,6 @@ class Robot:
         sensor= np.array(th["prox.horizontal"]) #get values from the sensors
         if sensor[0]>1000 or sensor[1]>1000 or sensor[2]>3000 or sensor[3]>1000 or sensor[4]>1000: # threshold a modifi� 
             self.state='LOCAL'
-            print('state',self.state)
             right=sensor[4]+sensor[3]   #values of the right sensors   
             left=sensor[0]+sensor[1]    #values of the left sensors
             if right>left:              #turn right if it feels the object on the left
@@ -206,8 +205,6 @@ class Robot:
         angle=ut.compute_angle(self.Pos[0:2],self.global_path[self.node+1])
 
         test=(m.pi+self.Pos[2]-angle)%(2*m.pi)-m.pi
-        test=abs(test)
-        print(test)
         if abs(test)<0.1:
             self.state='INIT'
             self.u[0]=0
