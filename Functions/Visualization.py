4d66b313360114221faabdfa89159726b5c6efca
Titouan Renard <titouan.renard@gmail.com>
2020-12-06 16:31:49 +0100

    visualization update

--- Functions/Visualization.py
+++ Functions/Visualization.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 import Functions.Utilities as ut
+import warnings
+from ipywidgets import interact, interactive, fixed, interact_manual
+
+warnings.filterwarnings("ignore")
 
 
 """
@@ -35,7 +39,7 @@ class  Visualization:
         return ret
 
 
-    def plotSensor(self, data):
+    def plotSensor(self, data,ptTime = False):
         if isinstance(data,str):
             data = self.loadData(data)
 
@@ -44,7 +48,10 @@ class  Visualization:
 
         for i in range(5):
             rawsensor = self.parseNone(sensor,i)
-            plt.plot(time,rawsensor)
+            plt.plot(time,rawsensor,color = 'blue')
+            if time is not False:
+                plt.axvline(x=time[ptTime],color = "black")
+                plt.plot(time[ptTime],rawsensor[ptTime],'ro', color = 'blue')
             plt.show()
 
 
@@ -58,14 +65,37 @@ class  Visualization:
             Parsing the data
         """
 
+        self.plotMotors(data)
+        
+        self.plotAlphaBeta(data)
 
+        self.plotRho(data)
+
+        
+        """
+            Plotting the data
+        """
+
+
+    def plotMotors(self, data,ptTime = False):
         time = self.loadKeySeq(data,'time')
-        visTimes = self.loadKeySeq(data,'vtime')
+
         ML = self.loadKeySeq(data,'ML')
         MR = self.loadKeySeq(data,'MR')
 
-        rawRho = self.loadKeySeq(data,'rho')
-        rho = self.parseNone(rawRho)
+        plt.plot(time,ML, color = 'red') 
+        plt.plot(time,MR, color = 'blue')
+
+        if ptTime is not False:
+            plt.axvline(x=time[ptTime],color = "black")
+            plt.plot(time[ptTime],MR[ptTime],'ro', color = 'blue') 
+            plt.plot(time[ptTime],ML[ptTime],'ro', color = 'red') 
+
+        plt.title("Motor Speed Values (left motor in red, right motor in blue) over time")
+        plt.show()
+
+    def plotAlphaBeta(self, data,ptTime = False):
+        time = self.loadKeySeq(data,'time')
 
         rawAlpha = self.loadKeySeq(data,'alpha')
         alpha = self.parseNone(rawAlpha)
@@ -73,25 +103,33 @@ class  Visualization:
         rawBeta = self.loadKeySeq(data,'beta')
         beta = self.parseNone(rawBeta)
 
-        """
-            Plotting the data
-        """
-
-        plt.plot(time,ML, 'bo', color = 'red') 
-        plt.plot(time,MR, 'bo', color = 'blue')
-        plt.title("Motor Speed Values (left motor in red, right motor in blue) over time")
-        plt.show()
+        if ptTime is not False:
+            plt.axvline(x=time[ptTime],color = "black")
+            plt.plot(time[ptTime],alpha[ptTime],'ro', color = 'blue') 
+            plt.plot(time[ptTime],beta[ptTime],'ro', color = 'red') 
 
         plt.plot(time,alpha, color = 'blue')
         plt.plot(time,beta, color = 'red')
         plt.title("Alpha (blue) and Beta (red) of ASTOLFI over time")
         plt.show()
 
+
+    def plotRho(self,data,ptTime = False):
+        time = self.loadKeySeq(data,'time')
+
+        rawRho = self.loadKeySeq(data,'rho')
+        rho = self.parseNone(rawRho)
+
+        
         plt.plot(time,rho, color = 'green')
+        
+        if ptTime is not False:
+            plt.axvline(x=time[ptTime],color = "black")
+            plt.plot(time[ptTime],rho[ptTime],'ro', color = 'green') 
         plt.title("Rho of ASTOLFI over time")
         plt.show()
 
-    def mapPlot(self,map=False,start=False,finish=False,visPos=False, kalPos = False):
+    def mapPlot(self,map=False,start=False,finish=False,visPos=False, kalPos = False, pos = False, path = False, rtext = False):
         #plotting parameters
         fsz = 8 # size of the plotted figure
         polyColor = "blue" # color of the map polygons
@@ -134,6 +172,10 @@ class  Visualization:
                     y.append(vertex[1])
                 plt.Polygon(poly,color=polyColor)
                 ax.fill(x,y,color = polyFillColor)  
+
+        if path is not False:
+            for i in  range(1, len(path)):
+                plt.plot([ path[i-1][0],path[i][0] ],[ path[i-1][1],path[i][1] ],color="black",linewidth=2)
         
         #Displaying start and finish
         if start is not False:
@@ -154,7 +196,19 @@ class  Visualization:
             for i in range(1,len(kalPos)):
                 if ( not isinstance(kalPos[i],bool) ) and ( not isinstance(kalPos[i-1],bool) ):
                     plt.plot([ kalPos[i-1][0],kalPos[i][0] ],[ kalPos[i-1][1],kalPos[i][1] ],color="blue",linestyle="dashed",linewidth=2)
-                        
+
+        if pos is not False:
+            posCircle = plt.Circle((pos[0],pos[1]),radius=2,color="green")
+            ax.add_artist(posCircle)
+            xs = (pos[0],pos[0]+math.cos(pos[2])*4.)
+            ys = (pos[1],pos[1]+math.sin(pos[2])*4.)
+
+            if rtext is not False:
+                ax.text(pos[0],pos[1],"Robot state = " + rtext,fontsize=11,color="black",weight="bold")
+            else:
+                ax.text(pos[0],pos[1],"Robot Position",fontsize=11,color="black",weight="bold")
+            plt.plot(xs,ys,linewidth = 5, color = "green")
+
         #displaying the map
         plt.show()
 
@@ -179,3 +233,42 @@ class  Visualization:
             self.plotAstolfi(data)
         elif plotType == 'sensor':
             self.plotSensor(data)
+
+    
+    def replay(self,data):
+
+        if isinstance(data,str):
+            data = self.loadData(data)
+        
+        def replayDisp(time  = 30, trace = True, motors = False, alphaBeta = False, rho = False,sensors = False):
+            kalPos = self.loadKeySeq(data,'fltPos')
+
+            start = (data[0]['visPos'][0],data[0]['visPos'][1])
+            goal = data[-1]['path'][-1]
+            mp = data[-1]['map']
+            
+            point = data[time]
+            pos = point['fltPos']
+            path = point['path']
+            if trace:
+                self.mapPlot(map = mp,start=start, finish = goal, kalPos=kalPos,pos = pos, path = path, rtext = str(point['state']) )
+            
+            if motors:
+                self.plotMotors(data,time)
+                
+            if alphaBeta:
+                self.plotAlphaBeta(data,time)
+                
+            if rho:
+                self.plotRho(data,time)
+                
+            if sensors:
+                self.plotSensor(data,time)
+            
+            print("position (according to kalmann filter) = \n" + str(pos) )
+            print("motor values, left = " + str(point['ML']) + " ; right = " + str(point['MR']))
+            print("ASTOLFI values, alpha = " + str(point['alpha']) + " ; beta = " + str(point['beta']) + " ; rho = " + str(point['rho']))
+            print("sensor values = " + str(point['sensor']) )
+            print("state of robot = " + str(point['state']))
+        
+        return interactive(replayDisp,time=(0,len(data) - 1 ) )
